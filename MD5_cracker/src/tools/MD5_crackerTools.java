38c9920abbf0ca0922c6e75365c69aa2afe73a9b
wes <wes@163.11.6.146>
2015-03-20 15:05:08 -0400

    Commented tool methods

--- MD5_cracker/src/tools/MD5_crackerTools.java
+++ MD5_cracker/src/tools/MD5_crackerTools.java
@@ -10,30 +10,39 @@ import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
  * @author wes
  */
 public class MD5_crackerTools {
-    
-    public static String getMD5hashString(String input) {
+    /**
+     * String to be turned into an MD5-hash string
+     * @param inputString
+     * @return String representation of inputString's MD5 hash
+     */
+    public static String getMD5hashString(String inputString) {
         HexBinaryAdapter adapter = new HexBinaryAdapter();
         
-        String MD5hashString = adapter.marshal(getMD5bytes(input));
+        String MD5hashString = adapter.marshal(getMD5bytes(inputString));
         MD5hashString = MD5hashString.trim();
         MD5hashString = MD5hashString.toLowerCase();
         
         return MD5hashString;
     }
     
+    /**
+     * String input is turned into an MD5-hash byte array
+     * @param MD5input
+     * @return the MD5 hashed representation of the input string
+     */
     private static byte[] getMD5bytes (String MD5input) {
         try {
-            MessageDigest mDig = MessageDigest.getInstance("MD5");
-            byte[] MD5bytes = mDig.digest(MD5input.getBytes("UTF-8"));
+            //  initializes a MessageDigest as an MD5 digest
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            //  digests the input byte array (encoded in UTF-8) into an MD5
+            //  byte array
+            byte[] MD5bytes = digest.digest(MD5input.getBytes("UTF-8"));
             
             return MD5bytes;
         }
         
-        catch (UnsupportedEncodingException ex) {
-            System.out.println("UTF-8 encoding is not supported in the current environment: " + ex.getMessage());
-            return null;
-        }
-        catch (NoSuchAlgorithmException ex) {
+        catch (UnsupportedEncodingException | 
+               NoSuchAlgorithmException ex) {
             return null;
         }   
     }    
--- MD5_cracker/test/tools/MD5_crackerToolsTest.java
+++ MD5_cracker/test/tools/MD5_crackerToolsTest.java
@@ -5,64 +5,26 @@
  */
 package tools;
 
-import java.io.UnsupportedEncodingException;
-import java.math.BigInteger;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
  * @author wes
  */
 public class MD5_crackerToolsTest {
-    
-    public MD5_crackerToolsTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
 
     @Test
     public void testGetMD5bytes() {
         System.out.println("Unit Test: getMD5bytes");
-        String MD5input = "1";
+        String MD5input = "12";
         System.out.println("Hashing: " + MD5input);
         
-        String expectedResult = "c4ca4238a0b923820dcc509a6f75849b";
+        String expectedResult = "c20ad4d76fe97759aa27a0c99bff6710";
         String actualResult = MD5_crackerTools.getMD5hashString(MD5input);
 
         System.out.println("Found   : " + actualResult);
         System.out.println("Expected: " + expectedResult);
 
         assert(expectedResult.equals(actualResult));    
-    }
-
-    /*
-    @Test
-    public void testGetMD5bytes_String() {
-        System.out.println("getMD5bytes");
-        String MD5input = "";
-        byte[] expResult = null;
-        byte[] result = MD5_crackerTools.getMD5bytes(MD5input);
-        assertArrayEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    @Test
-    public void testGetMD5bytes_byteArr() {
-        System.out.println("getMD5bytes");
-        byte[] MD5input = null;
-        byte[] expResult = null;
-        byte[] result = MD5_crackerTools.getMD5bytes(MD5input);
-        assertArrayEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-    */    
+    }   
 }
