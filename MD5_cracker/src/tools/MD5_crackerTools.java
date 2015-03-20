a247da82df1e494f1bb32c54ef2b4543459b82c6
wes <wes@163.11.6.146>
2015-03-20 13:31:43 -0400

    Added unit testing for the cracker tools
    User can use the tools to input a string and get a string back that is the MD5 hash of the input

--- MD5_cracker/src/tools/MD5_crackerTools.java
+++ MD5_cracker/src/tools/MD5_crackerTools.java
@@ -2,8 +2,8 @@ package tools;
 
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
-import java.security.MessageDigestSpi;
 import java.security.NoSuchAlgorithmException;
+import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
 
 /**
  *
@@ -11,19 +11,30 @@ import java.security.NoSuchAlgorithmException;
  */
 public class MD5_crackerTools {
     
-    public static byte[] getMD5bytes (String MD5input) {
-        byte[] MD5bytes = new byte[0];
+    public static String getMD5hashString(String input) {
+        HexBinaryAdapter adapter = new HexBinaryAdapter();
+        
+        String MD5hashString = adapter.marshal(getMD5bytes(input));
+        MD5hashString = MD5hashString.trim();
+        MD5hashString = MD5hashString.toLowerCase();
+        
+        return MD5hashString;
+    }
+    
+    private static byte[] getMD5bytes (String MD5input) {
         try {
             MessageDigest mDig = MessageDigest.getInstance("MD5");
-            byte[] MD5inputBytes = MD5input.getBytes("UTF-8");
-            mDig.digest(MD5inputBytes);
+            byte[] MD5bytes = mDig.digest(MD5input.getBytes("UTF-8"));
+            
+            return MD5bytes;
         }
-        catch (NoSuchAlgorithmException ex) {
-            System.out.println("No MD5 algorithm exists in the current environment: " + ex.getMessage());
-        }
+        
         catch (UnsupportedEncodingException ex) {
             System.out.println("UTF-8 encoding is not supported in the current environment: " + ex.getMessage());
+            return null;
         }
-        return MD5bytes;
-    }        
+        catch (NoSuchAlgorithmException ex) {
+            return null;
+        }   
+    }    
 }
